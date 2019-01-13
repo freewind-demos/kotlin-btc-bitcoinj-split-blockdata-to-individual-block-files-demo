3e9888c563caaf62ad90e7ec691df99b2c3bac98
freewind <nowindlee@gmail.com>
2019-01-14 00:04:54 +0800

    can stop and re-continue

--- src/main/kotlin/example/SplitToBlockFiles.kt
+++ src/main/kotlin/example/SplitToBlockFiles.kt
@@ -8,9 +8,34 @@ import org.bitcoinj.utils.BlockFileLoader
 import java.io.File
 import java.io.FileOutputStream
 
-val mainNetParams = MainNetParams()
-val splitFilesDir = File("./split-files")
-val blockChainFiles = listOf(File("./btc-data/blocks/blk00000.dat"))
+val mainNetParams = MainNetParams().apply { Context.getOrCreate(this) }
+
+//val splitFilesDir = File("./split-files")
+//val blockChainFiles = listOf(File("./btc-data/blocks/blk00000.dat"))
+val splitFilesDir = File("/Volumes/Mac2/Downloads/bitcoin-block-files")
+val allBlockchainFiles = File("/Volumes/Mac2/Downloads/Library/Application Support/Bitcoin/blocks").listFiles().filter { it.name.startsWith("blk") }
+
+val blockChainFiles = removeHandledFiles(allBlockchainFiles)
+
+fun removeHandledFiles(allBlockFiles: List<File>): List<File> {
+    val allSplitFiles = findAllIndividualBlockFiles(splitFilesDir)
+    val handledFiles = allBlockFiles.takeWhile { file ->
+        println("check handled file: $file")
+        val loader = BlockFileLoader(mainNetParams, listOf(file))
+        val block = loader.next()
+        val height = findBlockHeightByHash(allSplitFiles, block.hashAsString)
+        height != null
+    }
+
+    return if (handledFiles.isEmpty()) allBlockFiles else {
+        val startFile = handledFiles.last()
+        allBlockFiles.dropWhile { it != startFile }
+    }
+}
+
+fun findAllIndividualBlockFiles(dir: File): List<File> {
+    return dir.listFiles().toList().flatMap { it.listFiles().toList() }
+}
 
 val leadingMagicBytes = Hex.decode("f9beb4d91d010000")!!
 
@@ -23,6 +48,7 @@ fun buildFile(height: Int, prevHash: String?, thisHash: String): File {
 }
 
 fun writeToFile(block: Block, file: File) {
+    println("write to: $file")
     if (!file.parentFile.exists()) {
         file.parentFile.mkdirs()
     }
@@ -32,20 +58,23 @@ fun writeToFile(block: Block, file: File) {
     byteArrayStream.close()
 }
 
-fun calcHeightByPrevHash(prevHash: String): Int {
-    fun findPrevHeightHash(fileName: String): Int? {
+fun findBlockHeightByHash(allFiles: List<File>, hash: String): Int? {
+    fun findBlockHeightFromFile(fileName: String, hash: String): Int? {
         val (height, _, thisHash) = """(\d+):(\w+)-(\w+).block""".toRegex().matchEntire(fileName)!!.destructured
-        return if (thisHash == prevHash) height.toInt() else null
+        return if (thisHash == hash) height.toInt() else null
     }
 
-    val allFiles = splitFilesDir.listFiles().toList().flatMap { it.listFiles().toList() }
-    val prevHeight = allFiles.asSequence().map { it.name }.map { fileName -> findPrevHeightHash(fileName) }.find { it != null }!!
-    return prevHeight + 1
+    val height = allFiles.asSequence().map { it.name }.map { fileName -> findBlockHeightFromFile(fileName, hash) }.find { it != null }
+    return height
+}
+
+fun calcHeightByPrevHash(prevHash: String): Int {
+    val allFiles = findAllIndividualBlockFiles(splitFilesDir)
+    val prevHeight = findBlockHeightByHash(allFiles, prevHash)
+    return prevHeight!! + 1
 }
 
 fun main(args: Array<String>) {
-    Context.getOrCreate(mainNetParams)
-
     val loader = BlockFileLoader(mainNetParams, blockChainFiles)
 
     var height: Int? = null
@@ -63,5 +92,4 @@ fun main(args: Array<String>) {
         }
         height += 1
     }
-
 }
